5e06829d2762c62ade4444b0e38424e39ebd8d54
zrida1 <ragimovazrida@gmail.com>
2026-08-10 21:52:49 +0400

    create and implement FileController

---
+++ src/main/java/com/example/moviewreviewapplication/controller/FileController.java
@@ -0,0 +1,50 @@
+package com.example.moviewreviewapplication.controller;
+
+import com.example.moviewreviewapplication.service.FileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/movies")
+@Tag(name = "Movie Posters", description = "Movie poster upload and download APIs")
+public class FileController {
+
+    private final FileService fileService;
+
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @Operation(summary = "Upload movie poster")
+    @PostMapping("/{movieId}/poster")
+    public ResponseEntity<String> uploadPoster(
+            @PathVariable Long movieId,
+            @RequestParam("file") MultipartFile file) throws IOException {
+
+        String fileName = fileService.uploadMoviePoster(movieId, file);
+
+        return ResponseEntity.ok(
+                "Poster uploaded successfully: " + fileName
+        );
+    }
+
+    @Operation(summary = "Get movie poster")
+    @GetMapping("/{movieId}/poster")
+    public ResponseEntity<byte[]> getPoster(
+            @PathVariable Long movieId) throws IOException {
+
+        byte[] file = fileService.downloadMoviePoster(movieId);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline")
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(file);
+    }
+}
